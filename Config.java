754810971b86214438d1613d70a56c5184fd1de1
anurag-393 <anuragd393@gmail.com>
2021-11-05 13:46:48 +0530

    Theme Changed

--- Config.java
+++ Config.java
@@ -1,7 +1,27 @@
+import java.awt.Color;
+
 public class Config {
     public static final Level[] LEVELS = new Level[]{
-        new Level("Easy", 8, 10, 10),
-        new Level("Medium", 20, 20, 10),
+        new Level("Easy", 8, 10, 20),
+        new Level("Medium", 18, 14, 40),
         new Level("Hard", 40, 40, 20),
     };
+
+    public static final Color[] NUMBER_COLORS = new Color[]{
+        new Color(255, 255, 255), // 0
+        new Color(25, 118, 210), // 1
+        new Color(57, 142, 60), // 2
+        new Color(211, 48, 48), // 3
+        new Color(123, 31, 162), // 4
+        new Color(244, 67, 54), // 5
+        new Color(244, 67, 54), // 6
+        new Color(244, 67, 54), // 7
+        new Color(244, 67, 54), // 8
+    };
+
+    public static final Color BOARD_DARK_COLOR = new Color(142, 204, 57);
+    public static final Color BOARD_LIGHT_COLOR = new Color(167, 217, 72);
+    
+    public static final Color BOARD_DISABLED_DARK_COLOR = new Color(215, 184, 153);
+    public static final Color BOARD_DISABLED_LIGHT_COLOR = new Color(229, 194, 159);
 }
--- Game.java
+++ Game.java
@@ -1,5 +1,9 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.net.MalformedURLException;
@@ -8,16 +12,19 @@ import java.util.Stack;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import javax.swing.plaf.metal.MetalButtonUI;
 
 public class Game {
     JFrame frame;
     JButton[][] buttons;
     Cell[][] cells;
     Level selectedLevel;
+    JPanel boardPanel;
     
     static ImageIcon FLAG_ICON;
     static ImageIcon BOMB_ICON;
@@ -44,63 +51,129 @@ public class Game {
     Game() {   
         frame  = new JFrame();
 
-        selectedLevel = Config.LEVELS[0];
+        JPanel parentPanel = new JPanel();
+        JPanel gameDetailsPanel = new JPanel();
+
+        parentPanel.setLayout(new BorderLayout());
+
+        selectedLevel = null;
+        // String[] levels = {"Easy", "Medium", "Hard"}; 
+        JComboBox<String> difficulty = new JComboBox<String>();
+
+        for (Level level : Config.LEVELS) {
+             difficulty.addItem(level.getName());
+        }  
+        difficulty.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                Level previousLevel = selectedLevel;
+                selectedLevel = Config.LEVELS[difficulty.getSelectedIndex()];
+
+                if (previousLevel != selectedLevel) {
+                    if(boardPanel != null) {
+                        parentPanel.remove(boardPanel);
+                    }
+                    boardPanel =  initBoard(selectedLevel);
+                    parentPanel.add(boardPanel, BorderLayout.CENTER);
+                    parentPanel.revalidate();
+                }
+            }
+        });
+        difficulty.getItemListeners()[0].itemStateChanged(null);
+        
+        parentPanel.add(gameDetailsPanel, BorderLayout.NORTH);
+        gameDetailsPanel.add(difficulty);
+        frame.add(parentPanel);
+        
+        frame.setTitle("Minesweeper"); 
+        frame.setVisible(true);
+        frame.setSize(500, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+    }
+
+
+    JPanel initBoard(Level selectedLevel) {
         Board board = new Board(selectedLevel);
-        
-        JPanel panel = new JPanel();
-        
-        panel.setLayout(new GridLayout(selectedLevel.getRows(), selectedLevel.getCols()));
+
+                // JComboBox<String> difficulty = new JComboBox<String>(levels);
+
+        JPanel gamePanel = new JPanel();
+        gamePanel.setLayout(new GridLayout(selectedLevel.getRows(), selectedLevel.getCols()));
 
         cells = board.getCells();
-        
+
         buttons = new JButton[selectedLevel.getRows()][selectedLevel.getCols()];
         for (int i = 0; i < cells.length; i++) {
             for (int j = 0; j < cells[i].length; j++) {
                 Cell cell = cells[i][j];
 
                 JButton button = new JButton();
+                if(i % 2 == j % 2) {
+                    button.setBackground(Config.BOARD_DARK_COLOR);
+                } else {
+                    button.setBackground(Config.BOARD_LIGHT_COLOR);
+                }
+                button.setUI(new MetalButtonUI() {
+                    @Override
+                    protected Color getDisabledTextColor() {
+                        return Config.NUMBER_COLORS[cell.getBombsNearby()];
+                    }
+                });
                 button.addMouseListener(new MouseAdapter() {
                     @Override
                     public void mouseClicked(MouseEvent e) {
-                        if(button.isEnabled()) {
+                        if (button.isEnabled()) {
                             if (SwingUtilities.isRightMouseButton(e)) {
                                 cell.setFlagged(!cell.isFlagged());
                                 button.setIcon(cell.isFlagged() ? FLAG_ICON : null);
                                 button.setText(null);
-                            } else if(SwingUtilities.isLeftMouseButton(e)){
+                                if (cell.isFlagged()) {
+                                    // --
+                                } else {
+                                    // ++
+                                }
+                            } else if (SwingUtilities.isLeftMouseButton(e)) {
                                 Stack<Cell> neighbourCells = new Stack<>();
                                 Stack<JButton> neighbourButtons = new Stack<>();
 
                                 neighbourCells.push(cell);
                                 neighbourButtons.push(button);
 
-                                do  {
+                                do {
                                     Cell neighbourCell = neighbourCells.pop();
                                     JButton neighbourButton = neighbourButtons.pop();
 
                                     neighbourCell.setRevealed(true);
                                     neighbourButton.setEnabled(false);
                                     neighbourButton.setIcon(null);
+                                    neighbourButton.setDisabledIcon(null);
+                                    
+                                    int i = neighbourCell.getPosition().getGridx();
+                                    int j = neighbourCell.getPosition().getGridy();
+                                    neighbourButton.setBackground(i % 2 == j % 2 ? Config.BOARD_DISABLED_DARK_COLOR : Config.BOARD_DISABLED_LIGHT_COLOR);
 
-                                    if(neighbourCell.hasBomb()) {
-                                        neighbourButton.setText("B");
-                                        // neighbourButton.setIcon(BOMB_ICON);
+                                    if (neighbourCell.hasBomb()) {
+                                        // neighbourButton.setText("B");
+                                        neighbourButton.setIcon(BOMB_ICON);
+                                        neighbourButton.setDisabledIcon(BOMB_ICON);
                                         result(false);
 
-                                    } else if(neighbourCell.getBombsNearby() > 0) {
+                                    } else if (neighbourCell.getBombsNearby() > 0) {
                                         neighbourButton.setText(String.valueOf(neighbourCell.getBombsNearby()));
                                     } else {
                                         neighbourButton.setText("0");
 
-                                        for(Cell nextCell: neighbourCell.getNeighbours()) {
+                                        for (Cell nextCell : neighbourCell.getNeighbours()) {
                                             if (!nextCell.isRevealed()) {
                                                 neighbourCells.push(nextCell);
-                                                neighbourButtons.push(buttons[nextCell.getPosition().getGridx()][nextCell
-                                                        .getPosition().getGridy()]);
+                                                neighbourButtons
+                                                        .push(buttons[nextCell.getPosition().getGridx()][nextCell
+                                                                .getPosition().getGridy()]);
                                             }
                                         }
                                     }
-                                } while(!neighbourCells.isEmpty());
+                                } while (!neighbourCells.isEmpty());
 
                                 if (winCheck()) {
                                     result(true);
@@ -109,18 +182,14 @@ public class Game {
                         }
                     }
                 });
-                
+
                 buttons[i][j] = button;
-                panel.add(button);
+                gamePanel.add(button);
             }
-            
+
         }
 
-        frame.add(panel);
-        frame.setTitle("Minesweeper"); 
-        frame.setVisible(true);
-        frame.setSize(500, 500);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        return gamePanel;
     }
 
     boolean winCheck() {
@@ -160,18 +229,19 @@ public class Game {
                  for (int j = 0; j < cells[i].length; j++) {
                     Cell cell = cells[i][j];
                      if(cell.hasBomb()) {
-                         buttons[i][j].setText("B");
-                    } else if(cell.getBombsNearby() > 0) {
-                        buttons[i][j].setText(String.valueOf(cell.getBombsNearby()));                                    
-                    
+                         buttons[i][j].setIcon(BOMB_ICON);
+                         buttons[i][j].setDisabledIcon(BOMB_ICON);
+                     } else if(cell.getBombsNearby() > 0) {
+                        buttons[i][j].setText(String.valueOf(cell.getBombsNearby()));
                     } else {
-                        buttons[i][j].setText("0");                
+                        buttons[i][j].setText("0"); 
                     }
                 }
             }
             for (int i = 0; i < buttons.length; i++) {
                 for (int j = 0; j < buttons[i].length; j++) {
                     buttons[i][j].setEnabled(false);
+                    buttons[i][j].setBackground(i % 2 == j % 2 ? Config.BOARD_DISABLED_DARK_COLOR : Config.BOARD_DISABLED_LIGHT_COLOR);
                 }
             }
         }
